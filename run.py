36634ab80892c9634511bcee23817d06c1c35e68
Christopher Ly <Christopherly99@gmail.com>
2021-02-06 03:47:33 -0800

    Setup test target

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ import env_setup
 import etl_election
 import etl_news
 import eda
-from utils import get_project_root
+from utils import get_project_root, load_config
 
 
 def main(targets):
@@ -17,12 +17,21 @@ def main(targets):
     env_setup.auth()
     env_setup.make_datadir()
 
+    if 'all' in targets:
+        targets = ['data', 'eda']
+
     if 'data' in targets:
-        etl_news.get_news_rts()
+        config = load_config('config/news_params.json')
+        etl_news.get_news_data(**config)
         # etl_election.generate_dataset()
         # etl_election.rehydrate_tweets(100)
+
     if 'eda' in targets:
         eda.main()
+
+    if 'test' in targets:
+        config = load_config('config/test_params.json')
+        etl_news.get_news_data(**config)
 
 if __name__ == '__main__':
     targets = sys.argv[1:]
