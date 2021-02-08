b92fd20907006a82b84543bc9296ee1d65dfb2fc
shl636 <shl636@shl636-12371.pods.shl636.svc.cluster.local>
2021-02-07 20:54:26 -0800

    bug fix

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ sys.path.insert(0, 'src')
 import env_setup
 import etl_election
 import etl_news
-import eda
+# import eda
 import similarity
 import hashtags
 from utils import get_project_root, load_config
@@ -38,7 +38,8 @@ def main(targets):
 
 
         temp = hashtags.count_features([os.path.join(root, 'test', 'testdata', 'test_election.jsonl')])
-        similarity.compile_vectors(os.path.join(root, 'test', 'testdata'), temp)
+        news_vectors = similarity.compile_vectors(os.path.join(root, 'test', 'testdata'), temp)
+        # TODO: calculate pairwise similarity among news vectors
 
 if __name__ == '__main__':
     targets = sys.argv[1:]
