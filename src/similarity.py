1567fc8d31207ac5f84537d93161d541099831be
shl636 <shl636@shl636-30003.pods.shl636.svc.cluster.local>
2021-02-14 02:36:16 -0800

    embedding finished and demo

--- src/similarity.py
+++ src/similarity.py
@@ -2,18 +2,57 @@ import numpy as np
 import pandas as pd
 from src.stats import *
 import os
-#from sklearn.manifold import SpectralEmbedding
+from sklearn.manifold import SpectralEmbedding
 
 # takes in the hashtag vector from election, subsamples the hashtag space in usertime line with this vector
 # election_hts: the ht vector
 # user_timeline_fps: a list of file paths directing at user timeline jsons
-def subsample_hashtags(election_hts:pd.Series, user_timeline_fps:list):
+def subsample_hashtags(election_hts:pd.Series, user_timeline_fps:list, normalize = False):
+    '''
+    takes in the hashtag vector from election, subsamples the hashtag space in usertime line with this vecto
+    
+    Parameters
+    ----------
+    election_hts : pd.Series
+        a dictionary with keys to be news_station names and values to be hashtag count vectors.
+        
+    user_timeline_fps : str
+        the list of filepaths that refers to timeline files of one news station
+        
+    normalize: boolean
+        passed in from the caller function compile_vectors, decides whether to normalize the hashtag counts
+
+    Returns
+    -------
+    count_vector : np.ndarray
+        an array of the counts of the hashtag space
+   
+    
+    '''
     user_hts = count_features(user_timeline_fps, top_k = None, mode='hashtag')
     subspaced_hts = user_hts.reindex(election_hts.index, fill_value=0)
     return subspaced_hts.values
 
-# timeline_fp: path that contains {news_station}_{number}_users.jsonl
+
+# timeline_fp: 
 def compile_vectors(timeline_fp, vector):
+    '''
+    takes in the folder that contains user timeline jsons, constructs a dictionary with keys of news_station names 
+    and values of the hashtag vectors
+    
+    Parameters
+    ----------
+    timeline_fp : str
+        path that contains {news_station}_{number}_users.jsonl
+        
+    vector : pd.Series
+        a pandas series object that is the value counts of the hashtag space from election dataset
+
+    Returns
+    -------
+    result : dict
+        a dictionary with keys of news_station names and values of the hashtag vectors
+    '''
     files = [os.path.join(timeline_fp, file) for file in os.listdir(timeline_fp) if 'users.jsonl' in file]
     result = {}
     for f in files:
@@ -21,6 +60,7 @@ def compile_vectors(timeline_fp, vector):
         print(f)
         # ugly, but had to comply with the hashtag collection function :p
         user_jsons = [f]
+        # calculate the vector for this news station
         ht_vector = subsample_hashtags(vector, user_jsons)
         result[news_station] = ht_vector
     return result
@@ -32,15 +72,62 @@ def jaccard_similarity(news_vec1: np.ndarray, news_vec2: np.ndarray):
     # just aliasing
     v1 = news_vec1
     v2 = news_vec2
-    return np.sum(np.minimum(v1, v2)) / np.sum(np.maximum(v1, v2))
+    result = 1 - np.sum(np.minimum(v1, v2)) / np.sum(np.maximum(v1, v2))
+#     print(np.minimum(v1, v2), np.maximum(v1, v2))
+    return result
 
 
-# takes in a list of file_paths towards folder for user timeline tweet for every news station
-def construct_jaccard(news_stations:list):
-    pass
+def construct_jaccard(news_vectors:dict):
+    '''
+    constructs the nxn adjacency matrix (affinity matrix) from the result of function compile_vectors() 
+    
+    Parameters
+    ----------
+    news_vectors : dict
+        a dictionary with keys to be news_station names and values to be hashtag count vectors.
+
+    Returns
+    -------
+    news : list
+        the order of news stations from after which the index of matrix follows
+    
+    adjacency : np.ndarry
+        the adjacency matrix
+    
+    '''
+    news = list(news_vectors.keys())
+    n_news = len(news)
+    adjacency = np.zeros((len(news), len(news)))
+    # O(n^2) is bnd but we don't have a lot of news stations.
+    for r in range(adjacency.shape[0]):
+        for c in range(adjacency.shape[1]):
+            news1 = news[r]
+            news2 = news[c]
+#             if news1 == news2:
+#                 adjacency[r][c] = 1
+#             else:
+            adjacency[r][c] = jaccard_similarity(news_vectors[news1], news_vectors[news2])
+    
+    return news, adjacency
 
 
 # embed a graph by calculating laplacian eigenmap
-def embed(affinity_matrix):
-    pass
+def embed(affinity_matrix, n = 2):
+    '''
+    uses SpectralEmbedding class from sklearn to calculate the laplacian eigenmap.
+    
+    Parameters
+    ----------
+    affinity_matrix : np.ndarray
+        a dictionary with keys to be news_station names and values to be hashtag count vectors.
+
+    Returns
+    -------
+    vectors: list
+        a list of n-d coordinates for each news station. couple this with the news vector from the first return of 
+        construct_jaccard to plot meaningful graphs
+    
+    '''
+    lap_eigenmap = SpectralEmbedding(n, affinity='precomputed')
+    return lap_eigenmap.fit_transform(affinity_matrix)
 
--- src/stats.py
+++ src/stats.py
@@ -4,24 +4,24 @@ import numpy as np
 
 
 # Count the number of occurences of every hashtag in the JSON
-def hashtag_counts(json):
+def hashtag_counts(json, normalize = False):
     print(json)
     df = pd.read_json(json, lines = True)
     if len(df) == 0:
         return pd.Series([])
     ht = df['entities'].apply(lambda e: [x['text'] for x in e['hashtags']])
-    return pd.Series(ht.sum()).value_counts()
+    return pd.Series(ht.sum()).value_counts(normalize=normalize)
 
 
 # Count the number of posts every user has made in the JSON
-def user_counts(json):
+def user_counts(json, normalize = False):
     df = pd.read_json(json, lines=True)
     us = df['user'].apply(lambda x: x['screen_name'])
-    return us.value_counts()
+    return us.value_counts(normalize=normalize)
 
 
 # Count either hashtags or users in all available JSON files
-def count_features(jsons, top_k = None, mode = 'hashtag'):
+def count_features(jsons, top_k = None, mode = 'hashtag', normalize = False):
     # Decide whether to count hashtags or users
     if mode == 'hashtag':
         method = hashtag_counts
@@ -29,13 +29,13 @@ def count_features(jsons, top_k = None, mode = 'hashtag'):
         method = user_counts
         
     # Compile count of first JSON in list
-    total_series = method(jsons[0])
+    total_series = method(jsons[0], normalize)
     print(f'vc shape {total_series.shape}')
     
     if len(jsons) > 1:
         # Append counts to every subsequent JSON
         for json in jsons[1:]:
-            vc_series = method(json)
+            vc_series = method(json, normalize)
             total_series = total_series.add(vc_series, fill_value = 0)
             print(f'vc shape {total_series.shape}')
 
